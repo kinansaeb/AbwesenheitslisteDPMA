7daf978b6f708a37b47399db8f7c48a9237f49ba
KiSaeb <KiSaeb@10.3.49.216>
2017-06-07 12:54:21 +0200

    - to do list hinzugefügt und andere änderungen

--- src/de/dpma/abwesenheitsliste/view/manageUsersController.java
+++ src/de/dpma/abwesenheitsliste/view/manageUsersController.java
@@ -3,33 +3,16 @@ package de.dpma.abwesenheitsliste.view;
 import java.util.logging.Logger;
 
 import de.dpma.abwesenheitsliste.AL;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class searchController {
+public class manageUsersController {
 	static Logger log = Logger.getLogger(AL.class.getName());
 	AL gen = new AL();
 	public static Stage stage;
-
-	@FXML
-	public void closeButton(ActionEvent event) {
-		Platform.exit();
-	}
-
-	/**
-	 * @author Kinan Saeb
-	 *         <p>
-	 *         This method closes the whole application.
-	 *         <p>
-	 *         This method creates a new event handler for closing the whole
-	 *         application within a button-click
-	 * 
-	 */
 
 	public void backButton(ActionEvent event) {
 		try {
@@ -49,9 +32,9 @@ public class searchController {
 		}
 	}
 
-	public void createScheduleButton(ActionEvent event) {
+	public void addUserButton(ActionEvent event) {
 		try {
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createSchedule.fxml"));
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addUsers.fxml"));
 			log.info("Scene wird initalisiert");
 			Parent root = (Parent) fxmlLoader.load();
 
@@ -59,7 +42,7 @@ public class searchController {
 			stage.setScene(new Scene(root));
 			stage.setResizable(false);
 			stage.show();
-			stage.setTitle("Termin anlegen");
+			stage.setTitle("Benutzer hinzufügen");
 
 		} catch (Exception e) {
 			e.printStackTrace();
